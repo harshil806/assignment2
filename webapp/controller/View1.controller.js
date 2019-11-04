f399800e90bf26f8318d6ab7b2173bd582bdc98f
harshil806 <harshilpatwa806@gmail.com>
2019-11-04 13:19:36 +0000

    commit2

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("test_git2.demo_git2.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"test_git2/demo_git2/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
